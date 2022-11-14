92b63e453b3c923a9b957ce938cc4fea49064845
Faruk <109393466+farukbeygo@users.noreply.github.com>
2022-11-15 00:22:48 +0300

    Update implication.java

--- implication.java
+++ implication.java
@@ -1,13 +1,7 @@
 public class java_test {
+    // impication s.t. A implies B
     public static boolean sleepIn(boolean weekday, boolean vacation) {
         return (!weekday || vacation);
     }
 
-    public static void main(String[] args) {
-        Boolean A = true;
-        Boolean B = true;
-        Boolean C = false;
-        Boolean D = false;
-        System.out.println(sleepIn(D, B));
-    }
 }
